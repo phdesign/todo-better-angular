0201031b0334abeee94e4d58687f7f585efd3855
Paul Heasley <paul@phdesign.com.au>
2018-02-16 07:43:05 +1100

    Test skeleton

--- test/helpers.js
+++
@@ -1,7 +0,0 @@
-export function collapseWhitespace(text) {
-    return text.replace(/\s+/g, ' ').trim();
-}
-
-export function find(el, selector) {
-    return angular.element(el[0].querySelectorAll(selector));
-}
